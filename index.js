74fc58e5cf1774ce4ea5d014087a65b05d4703f1
sunnyghosh108 <sunnyghoshindra009@gmail.com>
2023-06-11 20:44:17 +0600

    added menu section

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const express =require('express');
 const cors =require('cors');
+//const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app =express();
 const port = process.env.PORT || 5000;
 
 // middleWare
- app.use(cors());
+ //app.use(cors());
  app.use(express.json());
 
-// const corsConfig = {
-//   origin: '*',
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE']
-//   }
-//   app.use(cors(corsConfig))
-//   app.options("", cors(corsConfig))
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+  app.options("", cors(corsConfig))
 
+// verifyJWT
+  // const verifyJWT =(req,res,next)=>{
+  //   const authorization =req.headers.authorization;
+  //   if(!authorization){
+  //       return res.status(401).send({error:true,message:'unauthorized access'});
+  //   }
+    //bearer token
+//     const token = authorization.split(' ')[1];
+
+//     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+//         if(err){
+//             return res.status(401).send({error:true,message:'unauthorized access'});
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+// }
 
 
 
@@ -40,15 +58,75 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-
+    const usersCollecton=client.db("summerCamp").collection("users");
     const menuCollecton=client.db("summerCamp").collection("menu");
+    const instructorCollecton=client.db("summerCamp").collection("instructor");
 
-  
+//Access token
+  //   app.post('/jwt',(req,res)=>{
+  //     const user = req.body;
+  //     const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+  //         expiresIn:'5h'
+  //     })
+  //     res.send({token})
+  //  })
+
+   
+     // Warning:use VerifyJWT before using VerifyAdmin
+    
+    //  const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email }
+    //   const user = await usersCollection.findOne(query);
+    //   if (user?.role !== 'admin') {
+    //     return res.status(403).send({ error: true, message: 'forbidden message' });
+    //   }
+    //   next();
+    // }
+   
+
+     //users related api
+     app.get('/users',async(req,res)=>{
+      const result =await usersCollecton.find().toArray();
+      res.send(result);
+     })
+     app.post('/users',async(req,res)=>{
+      const user = req.body;
+      console.log(user);
+      const query = {email: user.email}
+      const existingUser =await usersCollecton.findOne(query);
+      console.log('existinguser',existingUser)
+      if(existingUser){
+        return res.send({message:'user already exists'})
+      }
+      const  result= await usersCollecton.insertOne(user);
+      res.send(result);
+     });
+     app.patch('/users/admin/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id)};
+      const updateDoc ={
+          $set: {
+              role: 'admin'
+            },
+      };
+      const result =await usersCollecton.updateOne(filter,updateDoc);
+      res.send(result);
+        
+   })
+
+  //menu
     app.get('/menu',async(req,res)=>{
       const result =await menuCollecton.find().toArray();
       res.send(result);
     })
-    
+    //instructor
+    app.get('/instructor',async(req,res)=>{
+        const result =await instructorCollecton.find().toArray();
+        res.send(result);
+      })
+      
   
 
   
